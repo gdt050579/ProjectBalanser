83457a68b52f659667a92535a848b73eb0a07c8a
Gabriel LAZA <gabriellaza02@gmail.com>
2023-11-26 15:37:36 +0200

    feat: add fixer remove invalid projects

--- src/fixer/projects_preference/fixer.py
+++ src/fixer/projects_preference/fixer.py
@@ -1,7 +1,8 @@
 
 import copy
 
-from domain import StudentPreference
+import config
+from domain import StudentPreference, ProjectPreference, ProjectCategory, Project
 from fixer.fixer import Fixer
 from fixer.fix_error import FixError
 
@@ -12,12 +13,27 @@ class ProjectsPreferenceFixer(Fixer[list[StudentPreference]]):
 
         self.__students_preferences = students_preferences
 
+    def __remove_invalid_projects(self, project_preferences: list[ProjectPreference], project_category: ProjectCategory) -> list[ProjectPreference]:
+        max_id_project_category = config.MAXIM_ID_PROJECT_CATEGORY.get(project_category)
+
+        new_project_preferences = list()
+        for project_preference in project_preferences:
+            if 1 <= project_preference.project.id <= max_id_project_category:
+                new_project_preferences.append(project_preference)
+
+        return new_project_preferences
+
     def _fix(self) -> list[StudentPreference]:
         students_preferences = copy.deepcopy(self.__students_preferences)
 
         for student_preferences in students_preferences:
-            pass
+            projects_preferences = student_preferences.projects
 
+            for project_category in ProjectCategory:
+                project_preferences = projects_preferences[project_category]
+                project_preferences = self.__remove_invalid_projects(project_preferences, project_category)
+
+                projects_preferences[project_category] = project_preferences
 
         return students_preferences
 
