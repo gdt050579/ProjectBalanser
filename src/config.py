5b0f52a74e58f9e056f14e8d0e52cf3786202c1c
Gabriel LAZA <gabriellaza02@gmail.com>
2023-11-26 15:38:57 +0200

    feat: add maxim id project config

--- src/config.py
+++ src/config.py
@@ -1,8 +1,18 @@
 
+from domain import ProjectCategory
+
+
 DRYRUN = True
 
 
 FILE_NAME_STUDENTS_SOLUTION = "students.projects"
+
+
+MAXIM_ID_PROJECT_CATEGORY = {
+    ProjectCategory.A : 30,
+    ProjectCategory.B : 39,
+    ProjectCategory.C : 25
+}
 
 
 SOLVER_NO_ITERATIONS = 5000
